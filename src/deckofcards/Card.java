3ea63306ae2783176fd2b5d2bce7ef9b8f945586
jackjozwik <56364185+jackjozwik@users.noreply.github.com>
2020-01-19 19:57:52 -0500

    added test field to card class

--- src/deckofcards/Card.java
+++ src/deckofcards/Card.java
@@ -4,6 +4,7 @@ package deckofcards;
 public class Card {
     
     private final String[] suit = new String[]{"Hearts", "Spades", "Clubs", "Diamonds"};
+    private String testField;
     
     public Card(){
     
